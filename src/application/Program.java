d34e7bffed8db6259424c3562f3427bc2792ce34
Igotfbairros <igorfbairros@gmail.com>
2021-02-02 11:57:50 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.print("Heloo world");
+		
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 
 	}
 
